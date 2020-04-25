616209d35a420f91838304b4a0f42b0652d91c0d
joao luiz <64334495+Joao-Pereiraa@users.noreply.github.com>
2020-04-25 19:54:59 -0300

    Add files via upload

---
+++ atividade1.java
@@ -0,0 +1,65 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class atividade1 {
+	public static void main(String[] args) {
+		
+		String nome;
+		int idade ;
+		String cidade; 
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		
+		System.out.println("digite seu nome: ");
+		nome = leitor.next();
+		
+		
+		System.out.println("digite sua idade: ");
+		idade = leitor.nextInt();
+		
+		
+		System.out.println("digite a cidade onde mora: ");
+		cidade = leitor.next();
+	
+		
+		
+		System.out.println("boas vindas, "+nome);
+		System.out.println("sua idade é, "+idade +"anos" );
+		System.out.println("você mora em, "+cidade);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
