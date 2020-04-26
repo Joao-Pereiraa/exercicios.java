c8a76727013565563859b650a4e4f45a2b320967
joao luiz <64334495+Joao-Pereiraa@users.noreply.github.com>
2020-04-26 17:08:05 -0300

    Add files via upload

---
+++ atividade2.java
@@ -0,0 +1,52 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class atividade2 {
+	
+	public static void main(String[] args) {
+		
+		
+		int number1 ;
+		int number2 ;
+		
+
+		Scanner leitor = new Scanner(System.in);
+		
+		
+		System.out.println("digite o primeiro numero: ");
+		number1 = leitor.nextInt();
+		
+		System.out.println("digite o segundo numero: ");
+		number2 = leitor.nextInt();
+		
+		
+		System.out.println("soma:"+ (number1 + number2));
+		System.out.println("subtração:"+ (number1 - number2));
+		System.out.println("multiplicação:"+ (number1 * number2));
+		System.out.println("divisão:"+ (number1 / number2));
+		System.out.println("módulo:"+ (number1 % number2));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
